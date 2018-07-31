5170bedbc9a3379d113f5292fbd7fb229d34e892
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:17:59 +0300

    made various importations

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+
+from django.http import Http404
+from django.views import generic
+from braces.views import SelectRelatedMixin
+from . import models
+from . import forms
+from django.contrib.auth import get_user_model
 
 # Create your views here.
