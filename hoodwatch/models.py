c30a09f8fcd16a6e9483f88107d98971340a82bf
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:09:34 +0300

    Create a class of registering the number of occupants in a certain Hoodwatch

--- hoodwatch/models.py
+++ hoodwatch/models.py
@@ -6,3 +6,26 @@ from django.core.urlresolvers import reverse
 
 
 # Create your models here.
+User = get_user_model()
+reigster = template.Library
+
+
+class Hoodwatch(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+    slug = models.SlugField(allow_unicode=True,  unique=True)
+    location = models.CharField(max_length=140, blank=True, default='')
+    occupants = models.ManyToManyField(User, through='HoodwatchMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('hoodwatch:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['name']
+
