6bea987f81cc39874a705f7f98200ef1b79f4eb8
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:05:20 +0300

    Create a class for registering

--- hoodwatch/admin.py
+++ hoodwatch/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+class HoodwatchMember(admin.TabularInline):
+    model = models.HoodwatchMember
+
+
+admin.site.register(models.Hoodwatch)
