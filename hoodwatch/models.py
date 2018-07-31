293186f761af5e5fea225c3ce5ef20b201171b48
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:08:47 +0300

    made various importations

--- hoodwatch/models.py
+++ hoodwatch/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.text import slugify
+from django import template
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
