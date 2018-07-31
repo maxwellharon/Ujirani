2861f23e20360261ce96b8afa4852bcd165de8c8
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:20:07 +0300

    Create class of various user businesses

--- posts/views.py
+++ posts/views.py
@@ -17,3 +17,19 @@ class BusinessList(SelectRelatedMixin, generic.ListView):
     model = models.Business
     select_related = ('user', 'hoodwatch')
 
+class UserBusinesses(generic.ListView):
+    model = models.Business
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.business_user = User.objects.prefetch_related('businesses').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.business_user.businesses.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['business_user'] = self.business_user
+        return context
