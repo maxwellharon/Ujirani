964788fae5d678072252e51c395ee799541eb13a
maxwellharon <maxwellharon54@gmail.com>
2018-08-01 01:08:29 +0300

    linking of joinhood,leavehood,create hood and singlehood watch

--- hoodwatch/urls.py
+++ hoodwatch/urls.py
@@ -3,3 +3,11 @@ from . import views
 from django.conf.urls.static import static
 
 app_name = 'hoodwatch'
+
+urlpatterns = [
+    url(r'^$', views.ListHoodwatchs.as_view(), name='all'),
+    url(r'^new/$', views.CreateHoodwatch.as_view(), name='create'),
+    url(r'businesses/in/(?P<slug>[-\w]+)/$', views.SingleHoodwatch.as_view(), name='single'),
+    url(r'join/(?P<slug>[-\w]+)/$', views.JoinHoodwatch.as_view(), name='join'),
+    url(r'leave/(?P<slug>[-\w]+)/$', views.LeaveHoodwatch.as_view(), name='leave'),
+]
